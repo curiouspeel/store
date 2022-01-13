f18589c6bc39e74e9fe75c15e70a36ff86cf6200
curiouspeel <curiouspeel@gmail.com>
2022-01-13 20:05:30 +0800

    Store admin functionalities - add new products, delete products;

--- src/components/StoreFront.js
+++ src/components/StoreFront.js
@@ -1,24 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Product from './Product';
 
-export default function StoreFront(){
-
-    // Hardcoded products:
-    const products = [{
-        name: "Cheese",
-        description: "200g cheese block",
-        image: "https://res.cloudinary.com/dbfn5lnvx/image/upload/q_auto,w_300/v1580649404/react-tutorial/products/cheese.png"
-    }, {
-        name: "Milk",
-        description: "1L of milk",
-        image: "https://res.cloudinary.com/dbfn5lnvx/image/upload/q_auto,w_300/v1580649400/react-tutorial/products/milk.png"
-    }]
+export default function StoreFront() {
+    const [products, setProducts] = useState([]);
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [validationMsg, setValidationMsg] = useState("");
 
 
-    return(
-        <div className="store-front">
-            <Product details={products[0]}/>
-            <Product details={products[1]}/>
-        </div>
+    function handleFormSubmit(e){
+        e.preventDefault();
+
+        if(!name){
+            setValidationMsg("Please enter a name");
+            return;
+        }
+        if(!description){
+            setValidationMsg("Please enter a description");
+            return;
+        }
+        setProducts([...products,{
+            id: products.length +1,
+            name: name,
+            description: description
+        }]);
+        console.log(products);
+        setName("");
+        setDescription("");
+        setValidationMsg("");
+
+    }
+
+    function handleDelete(id){
+        setProducts(products.filter(product=> product.id!== id));
+    }
+
+
+    return (
+        <>
+            <form onSubmit={handleFormSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        id="name"
+                        type="text"
+                        placeholder="Enter the name"
+                        className="ui-textfield"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="description">Description:</label>
+                    <input
+                        id="description"
+                        type="text"
+                        placeholder="Enter the description"
+                        className="ui-textfield"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                </div>
+                <div className="form-footer">
+                    <div className="validation-msg">{validationMsg}</div>
+                    <input
+                        type="submit"
+                        className="ui-button"
+                        value="Add product"
+                    />
+                </div>
+            </form>
+            
+            {products.length<1 && <div><p>Add your first product</p></div>}
+
+            <ul className="store-front">
+                {products.map(product=> (<li key={product.id}>
+                        <Product details={product}/>
+                        <button
+                        onClick={()=>handleDelete(product.id)}
+                        >
+                            Delete
+                        </button>
+                    </li>)
+                )}
+            </ul>
+        </>
     );
 }
